043da0ac1c55b350412ab5fbf3d2603d44d84f22
mariusgeorgescu <georgescumarius@live.com>
2024-04-14 19:33:28 +0300

    wip

--- flake.nix
+++ flake.nix
@@ -14,18 +14,17 @@
       # TODO add missing arm to match standard systems
       #  right now purs-nix is only compatible with x86_64-linux
       systems = [ "x86_64-linux" "x86_64-darwin" ];
-      overlays = with inputs.ctl.overlays;
-        [
-          # adds easy-ps for CTL
-          purescript
-          # adds:
-          #  plutip-server
-          #  ogmios
-          #  kupo
-          runtime
-        ];
+      overlays = with inputs.ctl.overlays; [
+        # adds easy-ps for CTL
+        purescript
+        # adds:
+        #  plutip-server
+        #  ogmios
+        #  kupo
+        runtime
+      ];
     in utils.apply-systems { inherit inputs systems overlays; }
-    ({ system, pkgs ,... }@ctx:
+    ({ system, pkgs, ... }@ctx:
       let
         inherit (pkgs) nodejs;
         # TODO Use a default purs version from CTL
@@ -97,12 +96,14 @@
       "https://cache.iog.io"
       "https://cache.zw3rk.com"
       "https://hercules-ci.cachix.org"
+      "https://klarkc.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.tcp4.me:cmk2Iz81lQuX7FtTUcBgtqgI70E8p6SOamNAIcFDSew="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
       "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
+      "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
     ];
   };
 }
